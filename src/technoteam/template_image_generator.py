149b510e567c31e8b0c9e812c845fee9ce24bf4a
garethwalkom <walkga04@googlemail.com>
2020-03-05 21:04:09 +0100

    Update template_image_generator.py

--- src/technoteam/template_image_generator.py
+++ src/technoteam/template_image_generator.py
@@ -26,7 +26,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
-from technoteam import activex as ax
+import technoteam.activex as ax
 
 def get_no_displays():
     """
